336c4742536bb602e4ba68d54b0f31c5a0eda18a
nobishino <nobishii11@gmail.com>
2024-03-02 12:17:17 +0900

    Add new method returning iter.Seq to Period type(Draft)

--- period_sequence.go
+++ period_sequence.go
@@ -1,6 +1,10 @@
 package synchro
 
-import "github.com/nobishino/gocoro/iter"
+import (
+	"time"
+
+	"github.com/nobishino/gocoro/iter"
+)
 
 // Seq returns iterator for the periodical sequence.
 func (p periodical[T]) Seq() iter.Seq[Time[T]] {
@@ -12,4 +16,34 @@ func (p periodical[T]) Seq() iter.Seq[Time[T]] {
 			}
 		}
 	}
+}
+
+// Sequence returns iterator that yields Time[T] values at regular intervals
+// between the start and end times of the Period[T]. The interval is specified
+// by the next function argument.
+//
+// If start < end, the process will increase from start to end. In other words,
+// when the current value exceeds end, the iteration is terminated.
+func (p Period[T]) Sequence(next func(Time[T]) Time[T]) iter.Seq[Time[T]] {
+	compare := isNotAfter[T]
+	// p.start > p.end
+	if p.from.After(p.to) {
+		compare = isNotBefore[T]
+	}
+
+	return func(yield func(Time[T]) bool) {
+		for current := p.from; compare(current, p.to); current = next(current) {
+			if !yield(current) {
+				break
+			}
+		}
+	}
+}
+
+// SequenceDuration is a wrapper for the Sequence function.
+// The interval is specified by the time.Duration argument.
+func (p Period[T]) SequenceDuration(d time.Duration) iter.Seq[Time[T]] {
+	return p.Sequence(func(t Time[T]) Time[T] {
+		return t.Add(d)
+	})
 }
--- period_sequence_test.go
+++ period_sequence_test.go
@@ -63,3 +63,57 @@ func TestPeriodical_Seq(t *testing.T) {
 		}
 	})
 }
+
+func TestPeriodSequence(t *testing.T) {
+	want := []Time[tz.UTC]{
+		New[tz.UTC](2014, 2, 5, 0, 0, 0, 0),
+		New[tz.UTC](2014, 2, 6, 0, 0, 0, 0),
+		New[tz.UTC](2014, 2, 7, 0, 0, 0, 0),
+		New[tz.UTC](2014, 2, 8, 0, 0, 0, 0),
+	}
+
+	t.Run("Time[UTC] params", func(t *testing.T) {
+		period, err := NewPeriod[tz.UTC](
+			New[tz.UTC](2014, 2, 5, 0, 0, 0, 0),
+			New[tz.UTC](2014, 2, 8, 0, 0, 0, 0),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		sequence := period.SequenceDuration(24 * time.Hour)
+		got := make([]Time[tz.UTC], 0, len(want))
+		sequence(func(t Time[tz.UTC]) bool {
+			got = append(got, t)
+			return true
+		})
+
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Fatalf("(-want, +got)\n%s", diff)
+		}
+	})
+
+	t.Run("goroutine should terminated when breaked", func(t *testing.T) {
+		// NOTE: this test might be flaky and t.Skip() should be added.
+		period, err := NewPeriod[tz.UTC](
+			New[tz.UTC](2014, 2, 5, 0, 0, 0, 0),
+			New[tz.UTC](2014, 2, 8, 0, 0, 0, 0),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		begin := runtime.NumGoroutine()
+		sequence := period.SequenceDuration(24 * time.Hour)
+		got := make([]Time[tz.UTC], 0, len(want))
+		sequence(func(t Time[tz.UTC]) bool {
+			got = append(got, t)
+			return false
+		})
+		if diff := cmp.Diff(want[:1], got); diff != "" {
+			t.Errorf("(-want, +got)\n%s", diff)
+		}
+		last := runtime.NumGoroutine()
+		if begin < last {
+			t.Errorf("goroutine migit be leaked. begin: %d, last: %d", begin, last)
+		}
+	})
+}
